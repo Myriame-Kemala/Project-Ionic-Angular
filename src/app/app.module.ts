7cf35674e148b6fd1f3cd8b35eacbd9a84c01518
Myriame Kemala <Myriamekemala030597@gmail.com>
2021-06-02 17:09:21 +0100

    project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,26 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import{AppareilsPage} from '../pages/appareils/appareils'
+import{SingleAppareilPage} from '../pages/appareils/single-appareil/single-appareil'
+import{SettingsPage} from '../pages/settings/settings'
+import{TabsPage} from '../pages/tabs/tabs'
+import { AppareilsService } from '../services/appareils.service';
+import { OptionsPage } from '../pages/options/options';
+import { AppareilFormPage } from '../pages/appareil-form/appareil-form';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AppareilsPage,
+    SingleAppareilPage,
+    SettingsPage,
+    TabsPage, 
+    OptionsPage,
+    AppareilFormPage
+  
   ],
   imports: [
     BrowserModule,
@@ -19,9 +34,16 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AppareilsPage,
+    SingleAppareilPage,
+    SettingsPage,
+    TabsPage,
+    OptionsPage,
+    AppareilFormPage
   ],
   providers: [
+    AppareilsService,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
---
+++ src/pages/appareils/appareils.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { SingleAppareilPage } from './single-appareil/single-appareil';
+import { Appareil } from '../../models/Appareil';
+import { AppareilsService } from '../../services/appareils.service';
+import { MenuController } from 'ionic-angular';
+import { AppareilFormPage } from '../appareil-form/appareil-form';
+
+@Component({
+  selector: 'page-appareils',
+  templateUrl: 'appareils.html'
+})
+export class AppareilsPage {
+
+  appareilsList: Appareil[];
+
+  constructor(private modalCtrl: ModalController,
+              private appareilsService: AppareilsService,
+              private menuCtrl: MenuController,
+              private navCtrl: NavController) {}
+
+  ionViewWillEnter() {
+    this.appareilsList = this.appareilsService.appareilsList.slice();
+  }
+
+  onLoadAppareil(index: number) {
+    let modal = this.modalCtrl.create(SingleAppareilPage, {index: index});
+    modal.present();
+  }
+  onToggleMenu() {
+    this.menuCtrl.open();
+  }
+
+  onNewAppareil() {
+    this.navCtrl.push(AppareilFormPage);
+}
+}
